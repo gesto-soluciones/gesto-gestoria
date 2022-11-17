678529a248ee52cccd59f5f1f20ac6c624bfd4b1
NiceTwice <onlynicetwice@gmail.com>
2022-11-16 21:52:12 -0300

    v0.9

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,6 @@ const Footer = () => {
         <>
             <section className="text-slate-800/80 w-full mt-32 pt-16 pb-4 bg-slate-100 text-white">
                 <div className="flex w-full gap-4 justify-center">
-                    <a href="#contact" target="_blank" className="rounded-full p-3 hover:bg-main hover:text-white bg-white text-main cursor-pointer transition-colors shadow-md">
-                        <RiFacebookFill size={25}/>
-                    </a>
                     <a href="https://www.instagram.com/gestosoluciones/" target="_blank" rel="noreferrer" className="rounded-full p-3 hover:bg-main hover:text-white bg-white text-main cursor-pointer transition-colors shadow-md">
                         <RiInstagramLine size={25}/>
                     </a>
--- components/Nav.js
+++ components/Nav.js
@@ -86,41 +86,45 @@ const Nav = () => {
                             Contacto
                         </a>
                         <div className="flex mt-2 gap-x-4">
-                            <a className="rounded-full p-2 bg-secondary hover:bg-white hover:text-secondary transition-colors cursor-pointer"><RiFacebookFill size={20}/></a>
+                            <a href="mailto:gesto.solucionesok@gmail.com" className="rounded-full p-3 hover:bg-main hover:text-white bg-white text-main cursor-pointer transition-colors shadow-md">
+                                <RiMailLine size={20}/>
+                            </a>
                             <a href="https://www.instagram.com/gestosoluciones/" target="_blank" rel="noreferrer" className="rounded-full p-2 bg-secondary hover:bg-white hover:text-secondary transition-colors cursor-pointer"><RiInstagramLine size={20}/></a>                               
                         </div>
                     </div>
                 </motion.div>
                 <div className="md:flex hidden font-sans gap-x-4 items-center">
-                        <a
-                            href="#services" 
-                            onClick={ () => setMenu(menu => !menu)} 
-                            className="text-white/80 hover:text-white cursor-pointer"
-                        >
-                            Servicios
-                        </a>
-                        <a
-                            href="#details" 
-                            onClick={ () => setMenu(menu => !menu)} 
-                            className="text-white/80 hover:text-white cursor-pointer"
-                        >
-                            Detalles
-                        </a>
-                        <a
-                            href="#testimonials" 
-                            onClick={ () => setMenu(menu => !menu)} 
-                            className="text-white/80 hover:text-white cursor-pointer"
-                        >
-                            Clientes
-                        </a>
-                        <a
-                            href="#contact" 
-                            onClick={ () => setMenu(menu => !menu)} 
-                            className="text-white/80 hover:text-white cursor-pointer"
-                        >
-                            Contacto
-                        </a>
-                    <a className="rounded-full p-1 bg-secondary hover:bg-white hover:text-secondary transition-colors cursor-pointer"><RiFacebookFill size={15}/></a>
+                    <a
+                        href="#services" 
+                        onClick={ () => setMenu(menu => !menu)} 
+                        className="text-white/80 hover:text-white cursor-pointer"
+                    >
+                        Servicios
+                    </a>
+                    <a
+                        href="#details" 
+                        onClick={ () => setMenu(menu => !menu)} 
+                        className="text-white/80 hover:text-white cursor-pointer"
+                    >
+                        Detalles
+                    </a>
+                    <a
+                        href="#testimonials" 
+                        onClick={ () => setMenu(menu => !menu)} 
+                        className="text-white/80 hover:text-white cursor-pointer"
+                    >
+                        Clientes
+                    </a>
+                    <a
+                        href="#contact" 
+                        onClick={ () => setMenu(menu => !menu)} 
+                        className="text-white/80 hover:text-white cursor-pointer"
+                    >
+                        Contacto
+                    </a>
+                    <a href="mailto:gesto.solucionesok@gmail.com" className="rounded-full p-3 hover:bg-main hover:text-white bg-white text-main cursor-pointer transition-colors shadow-md">
+                        <RiMailLine size={15}/>
+                    </a>
                     <a href="https://www.instagram.com/gestosoluciones/" target="_blank" rel="noreferrer" className="rounded-full p-1 bg-secondary hover:bg-white hover:text-secondary transition-colors cursor-pointer -ml-2"><RiInstagramLine size={15}/></a>
                 </div>
             </div>
